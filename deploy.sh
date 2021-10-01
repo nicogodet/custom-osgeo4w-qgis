3371f0b9e9e93916f276a5681bdfecc38b8d69c6
nicogodet <nicolas.godet@outlook.fr>
2021-10-01 18:01:18 +0200

    ISL work

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ cd qgis-isl
 
 SETUP_TEXT=$(cat setup.hint)
 
-PACKAGE_DIR="/mnt/l/Qgis/DEPLOY/http%3a%2f%2fwww.norbit.de%2fosgeo4w%2fv2%2f"
+PACKAGE_DIR="/mnt/l/Qgis/DEPLOY/sources"
 
 echo "-Target package directory : 
 $PACKAGE_DIR
@@ -41,7 +41,7 @@ mkdir -p "$PACKAGE_DIR/x86_64/release/qgis/qgis-isl/"
 
 cd - 
 
-cp -p qgis-isl-$VERSION.tar.bz2 $PACKAGE_DIR/x86_64/release/qgis/qgis-isl/
+sudo cp -p qgis-isl-$VERSION.tar.bz2 $PACKAGE_DIR/x86_64/release/qgis/qgis-isl/
 
 # md5 and size  
 MD5=$(md5sum qgis-isl-$VERSION.tar.bz2 | awk -F'[ ]'  '{print $1}')
@@ -49,20 +49,28 @@ size=$(stat -c "%s" qgis-isl-$VERSION.tar.bz2)
 
 # adds metadata into setup.ini, from setup.hint template
 
-echo -e "
-- Modification of setup.ini"
+echo -e "- Modification of setup.ini"
+
+rm -f setup.ini
+cp -p $PACKAGE_DIR/x86_64/setup.ini setup.ini
+chmod +w setup.ini
 
 # deletes previous entry
-sed -i  '/@ qgis-isl/,+8d;' $PACKAGE_DIR/x86_64/setup.ini
+sed -i '/@ qgis-isl/,+8d;' setup.ini
 
 # append to the end of the file
-
 echo "@ qgis-isl
 $SETUP_TEXT $size $MD5 
-" >>  $PACKAGE_DIR/x86_64/setup.ini 
+" >>  setup.ini 
+
+sudo cp -fp setup.ini $PACKAGE_DIR/x86_64/setup.ini
 
 echo -e "--------"
 
 echo -e "** Package deployed **"
 
 echo -e "--------"
+
+sudo cp -p install/install_QGIS.bat $PACKAGE_DIR/../
+sudo cp -p install/osgeo4w-setup.exe $PACKAGE_DIR/../
+
---
+++ install/install_QGIS.bat
@@ -0,0 +1,13 @@
+.\osgeo4w-setup.exe `
+  --menu-name "QGIS ISL" `
+  --root "C:\OSGeo4W" `
+  --advanced  `
+  --quiet-mode   `
+  --local-install `
+  --local-package-dir "\\islsudouest\users\Godet\Qgis\DEPLOY\http%3a%2f%2fwww.norbit.de%2fosgeo4w%2fv2%2f" `
+  --autoaccept  `
+  --delete-orphans `
+  --upgrade-also `
+  -C Libs `
+  -C Desktop `
+  -C Commandline_Utilities
